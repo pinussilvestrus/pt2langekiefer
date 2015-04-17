765d969988bb4ce756dbf6ecc887c84959ff9a27
Niklas Kiefer <niklas.kiefer@student.hpi.uni-potsdam.de>
2015-04-17 19:26:31 +0200

    solve nr 2a and 2b as good as possible

--- 1/programmrahmen/fibonacci_iterative.cpp
+++ 1/programmrahmen/fibonacci_iterative.cpp
@@ -8,8 +8,26 @@ int fibonacci(int number)
 	// ToDo: Exercise 2.c - count number of calculation steps
 
 	// ToDo: Exercise 2.b - retun 0 on bad arguments
+	if (number <= 0 || number > 32767) { //32767 max for int
+		return 0;
+	}
 
 	// ToDo: Exercise 2.b - retrieve nth fibonacci number iteratively
+	
+	if (number == 1) {
+		return 1;
+	}
+	else {
+		int temp = 0;
+		int a = 1; //for fib(1);
+		int b = 1; //for fib(2);
+		for (int i = 3;i<=number;i++) {
+			temp = a + b;
+			a = b;
+			b = temp;
+		}
+		return temp;
+	}
 
 	return 0;
 }
--- 1/programmrahmen/fibonacci_recursive.cpp
+++ 1/programmrahmen/fibonacci_recursive.cpp
@@ -8,8 +8,17 @@ int fibonacci(int number)
 	// ToDo: Exercise 2.c - count number of calculation steps
 
 	// ToDo: Exercise 2.b - retun 0 on bad arguments
+	if (number <= 0 || number > 32767) { //32767 max for int
+		return 0;
+	}
 
 	// ToDo: Exercise 2.a - retrieve nth fibonacci number recursively
+	if (number==1) {
+		return 1;
+	}
+	else {
+		return fibonacci(number - 1) + fibonacci(number - 2);
+	}
 
 	return 0;
 }
