788c4fdf9dd84e30f6a2f9b6dfed60ced6848629
Niklas Kiefer <niklas.kiefer@student.hpi.uni-potsdam.de>
2015-05-10 18:56:45 +0200

    solve 2.2 and waiting for tutors fixes

--- 3/programmrahmen/exceptions.cpp
+++ 3/programmrahmen/exceptions.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstring>
 #include <sstream>
 #include <cfloat>
 #include <iomanip>
@@ -12,7 +13,7 @@ std::tm stringToTime(std::string date)
 {
     std::tm t;
     std::istringstream ss(date);
-    ss >> strptime(date.c_str(), "%d.%m.%Y", &t);
+    //ss >> strptime(date.c_str(), "%d.%m.%Y", &t);
     
     if(t.tm_year < 105 || t.tm_year > 115)
         throw std::logic_error("Year should be between 2005 and 2015");
@@ -28,17 +29,71 @@ struct FormatException
 
 void parseLine(std::string line, int lineNum)
 {
+	
+	if (lineNum == 0){
+		return; // don't parse header
+	}
     const std::string fieldNames[3] = { "Date", "Temperature", "Rainfall" };
+	const std::string delimiter = ";"; //for parsing
     
     // todo 3.2b: parse a given line, check dates by calling stringToTime, check temperature/rainfall by calling std::stof. Catch all exceptions thrown by these methods. If there have been any exceptions, aggregate all necessary information into an instance of FormatException and throw that instance.
     
+	/******************** parsing *******/
+	std::string lineDate = line.substr(0,line.find(delimiter)); // first
+	line.erase(0,line.find(delimiter)+1);
+	std::string lineTemperature = line.substr(0,line.find(delimiter)); // second
+	line.erase(0,line.find(delimiter)+1);
+	std::string lineRainfall = line; // third
+	//std::cout << lineDate << " " << lineTemperature << " " << lineRainfall << "\n";
+	
+	FormatException excp;
+	excp.m_actLine = lineNum;
+	int numOfFailures = 0;
+	
+	/***** Date ***/
+	try {
+		stringToTime(lineDate);
+	} catch (std::logic_error e){
+		excp.m_actFields += fieldNames[0] + ",";
+		numOfFailures++;
+	}
+	
+	/***** Temperature **/
+	try {
+		//std::stof(lineTemperature);
+	} catch(std::invalid_argument e){
+		excp.m_actFields += fieldNames[1] + ",";
+		numOfFailures++;
+	}
+	
+	/******* Rainfall **/
+	
+	try {
+		//std::stof(lineRainfall);
+	} catch (std::invalid_argument e){
+		excp.m_actFields += fieldNames[2] + ",";
+		numOfFailures++;
+	}
+	if (numOfFailures>0){
+		throw excp;
+	}
+	
 }
 
 // todo 3.2c..
 void writeOutFormatException(const FormatException & e)
 {
     // todo 3.2d: export information (i.e., line number + invalid data fields) about exception to a logfile.
+	// file.exceptions ( std::ifstream::failbit | std::ifstream::badbit );
+	try {
+		std::fstream file("./exceptions.log",std::ios_base::app | std::ios::out);
+		file << e.m_actLine << " " << e.m_actFields << "\n";
+		file.close();
+	}
     // todo 3.2d: catch ios_base::failure
+	catch (std::ios_base::failure e) {
+		std::cerr << "Exception opening/reading/closing file\n";
+	}
 }
 
 void checkData(std::string path)
@@ -48,7 +103,28 @@ void checkData(std::string path)
     std::ifstream file;
     
     // todo 3.2a: open file + read each line + call parseLine function (catch ifstream::failure)
-    // todo 3.2c: read each line + call parseLine function (catch FormatException) + count valid + invalid lines
+	// file.exceptions ( std::ifstream::failbit | std::ifstream::badbit );
+	try {
+		file.open(path);
+
+		std::string line;
+		int lineNumber = 0;
+		while (std::getline(file,line)){
+			// todo 3.2c: read each line + call parseLine function (catch FormatException) + count valid + invalid lines
+			try {
+				//std::cout << line<<"\n";
+				parseLine(line,lineNumber);
+				validLines++;
+			} catch(FormatException e){
+				writeOutFormatException(e);
+				invalidLines++;
+			}
+			lineNumber++;
+		}
+		file.close();
+	}catch (std::ifstream::failure e) {
+		std::cerr << "Exception opening/reading/closing file\n";
+	}
     
     std::cout << "valid lines: " << validLines << " - invalid lines: " << invalidLines << std::endl;
 }
