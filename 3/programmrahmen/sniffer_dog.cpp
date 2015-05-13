744e7590e6655aad5fd49ed694fa5e40ec9abde0
Niklas Kiefer <niklas.kiefer@student.hpi.uni-potsdam.de>
2015-05-13 15:46:25 +0200

    solve task 3.3

--- 3/programmrahmen/sniffer_dog.cpp
+++ 3/programmrahmen/sniffer_dog.cpp
@@ -6,7 +6,76 @@
 // todo 3.3: Calculate minimum time needed for both dogs to return 
 int minTimeDogTraining(int searchValueX, int searchValueY, const std::vector<int> &boxes)
 {
-	return -1;
+	int num1 = -1;
+	int num2 = -1;
+	int num3 = -1;
+	int num4 = -1;
+	
+	/**** dog x and y start left **/
+	int foundX = 0; //time to find searchValueX;
+	int foundY = 0; //time to find searchValueY;
+	while ((foundX==0) && (foundY==0)) {
+		for (int i=0;i<boxes.size();i++){
+			if (searchValueX==boxes[i]){
+				foundX=i+1;
+			}
+			if (searchValueY==boxes[i]){
+				foundY=i+1;
+			}
+		}
+	}
+	num1 = std::max(foundX,foundY);
+	/***********************/
+	
+	/**** dog x and y start right**/
+	foundX = 0;
+	foundY = 0;
+	while ((foundX==0) && (foundY==0)) {
+		for (int i=0;i<boxes.size();i++){
+			if (searchValueX==boxes[boxes.size()-i-1]){
+				foundX=i+1;
+			}
+			if (searchValueY==boxes[boxes.size()-i-1]){
+				foundY=i+1;
+			}
+		}
+	}
+	num2 = std::max(foundX,foundY);
+	/*******************************/
+	
+	/***** dog x starts at left and y at right **/
+	foundX = 0;
+	foundY = 0;
+	while ((foundX==0) && (foundY==0)) {
+		for (int i=0;i<boxes.size();i++){
+			if (searchValueX==boxes[i]){
+				foundX=i+1;
+			}
+			if (searchValueY==boxes[boxes.size()-i-1]){
+				foundY=i+1;
+			}
+		}
+	}
+	num3 = std::max(foundX,foundY);
+	/*******************************/
+	
+	/***** dog x starts at right and y at left **/
+	foundX = 0;
+	foundY = 0;
+	while ((foundX==0) && (foundY==0)) {
+		for (int i=0;i<boxes.size();i++){
+			if (searchValueX==boxes[boxes.size()-i-1]){
+				foundX=i+1;
+			}
+			if (searchValueY==boxes[i]){
+				foundY=i+1;
+			}
+		}
+	}
+	num4 = std::max(foundX,foundY);
+	/*******************************/
+	
+	return std::min(num1,std::min(num2,std::min(num3,num4)));
 }
 
 int main(int argc, char * argv[])
