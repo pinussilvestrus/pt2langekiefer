a4b297165943532f16f6c66fa8c8f40a90bbe167
Niklas Kiefer <niklas.kiefer@student.hpi.uni-potsdam.de>
2015-05-19 19:45:43 +0200

    solve task 4.2

--- 4/programmrahmen/functionwrapping.cpp
+++ 4/programmrahmen/functionwrapping.cpp
@@ -11,7 +11,7 @@ struct CommandLineParameter
 		, m_centeredTriangular(false)
 		, m_powSum(false)
 	{
-		for (int i = 1; i < argc; i += 2)
+		for (int i = 1; i < argc; i++)
 		{
 			if (!strcmp(argv[i], "-fib"))
 			{
@@ -33,18 +33,29 @@ struct CommandLineParameter
 	bool m_powSum;
 };
 
-// ToDo 4.2a - Calculate and return the fibonacci number for the given input value
+// ToDo 4.2a - Calculate and return the fibonacci number for the given input value, iterative
 int fibonacci(int number)
 {
-	if (number <= 0){
+	if (number <= 0 ){
 		return 0;
 	}
-
-	if (number==1) {
+	
+	if (number == 1) {
 		return 1;
 	}
 	else {
-		return fibonacci(number - 1) + fibonacci(number - 2); //recursive
+		int temp = 0;
+		int a = 0; 
+		int b = 1;
+		for (int i = 2;i<=number;i++) {
+			temp = a + b;
+			a = b;
+			b = temp;
+		}
+		if (temp <= 32767) { //filter bad output
+			return temp; 
+		}
+		else { return 0; }
 	}
 }
 
@@ -67,7 +78,7 @@ int powSum(int number, int exp)
 	
 	int sum = 0;
 	for (int i = 1; i <= number; i++){
-		sum+=i^exp;
+		sum+=std::pow(i,exp);
 	}
 	return sum;
 }
@@ -75,6 +86,12 @@ int powSum(int number, int exp)
 // ToDo 4.2c - Call each function inside the functions vector. Print the results via std::cout
 void applyFuntions(std::vector<std::function<int(int)>> & functions, int number)
 {
+	std::cout << number << ": ";
+	std::vector<std::function<int(int)>>::size_type i;
+	for(i = 0; i != functions.size(); i++) {
+		int result = functions[i](number);
+		std::cout << result << " ";
+	}
 	std::cout << std::endl;
 }
 
@@ -86,14 +103,25 @@ int main(int argc, char * argv[])
 	if (cmd.m_fibonacci)
 	{
 		// ToDo 4.2b - Add the fibonacci function directly to the functions vector
+		functions.push_back(fibonacci);
 	}
 	if (cmd.m_centeredTriangular)
 	{
 		// ToDo 4.2b - Add a lambda expression that calls the centeredTriangular function to the functions vector
+		std::function<int (int)> ct = [](int i) {return centeredTriangular(i);};
+		functions.push_back(ct);
 	}
 	if (cmd.m_powSum)
 	{
 		// ToDo 4.2b - Add the powSum function using a std::bind expression to the functions vector. Use a constant value (e.g., 2) for the exp argument 
+		using std::placeholders::_1;
+		std::function<int (int)> ps = std::bind(powSum,_1,2);
+		functions.push_back(ps);
+	}
+	
+	if (functions.size()==0){
+		std::cout<<"Please choose at least one option: -fib (fibonacci), -ct (centeredTriangular), -pow (powSum)";
+		return 1;
 	}
 
 	for (int i = 1; i < 45; i++)
