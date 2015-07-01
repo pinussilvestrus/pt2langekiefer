8b984f745e27b3310a7602fe963f4239adbab610
Niklas Kiefer <niklas.kiefer@student.hpi.uni-potsdam.de>
2015-07-01 20:53:29 +0200

    add new skeleton code for extratask

--- 7/programmrahmen/producerconsumer.cpp
+++ 7/programmrahmen/producerconsumer.cpp
@@ -5,70 +5,130 @@
 #include <queue>
 #include <list>
 #include <random>
+#include <mutex>
+#include <string>
 
 
 using namespace std::chrono;
 
-
 // uint64_t is defined in stdint.h -> typedef unsigned long long uint64_t
 // -> it's a standardized abbreviation/shortcut for unsigned long long for convinience
 
+auto mainThreadId = std::this_thread::get_id();
 
-// Protoypical Item class - could conatin any application specific data..
+// print a text to console (thread safe)
+void printToConsole(const std::string &string)
+{
+	static std::mutex coutMutex;
+
+	//do this calculation outside of locked mutex, so locked time is shorter
+	std::string indentation = "";
+	if (std::this_thread::get_id() != mainThreadId)
+	{
+		indentation = "\t\t\t";
+	}
+
+	coutMutex.lock();
+	std::cout << indentation << string << std::endl;
+	coutMutex.unlock();
+}
+
+//Implement a thread safe queue, because we don't know if underlying class is thread safe
+template<typename T, class Container = std::deque<T>>
+class ThreadSafeQueue
+{
+public:
+	bool empty() const
+	{
+		queueMutex_.lock();
+		bool isEmpty = queue_.empty();
+		queueMutex_.unlock();
+
+		return isEmpty;
+	}
+
+	//normally you would call front() and then pop() on normal queue, but because of possible changes between both calls, we combine them
+	T pop_front()
+	{
+		queueMutex_.lock();
+		auto item = queue_.front();
+		queue_.pop();
+		queueMutex_.unlock();
+
+		return item;
+	}
+
+	typename Container::size_type size() const
+	{
+		queueMutex_.lock();
+		auto size = queue_.size();
+		queueMutex_.unlock();
+
+		return size;
+	}
+
+	void push(const T& value)
+	{
+		queueMutex_.lock();
+		queue_.push(value);
+		queueMutex_.unlock();
+	}
+
+protected:
+	std::queue<T, Container> queue_;
+	mutable std::mutex queueMutex_; //Mutex to prevent data races on queue
+};
+
+// Prototypical Item class - could contain any application specific data.
 class Item
 {
 public:
 	Item()
-		: m_uid{ s_counter++ }
-		, m_spawntime{ system_clock::now() }
+		: uid_{counter_++}
+		, spawntime_{system_clock::now()}
 	{
-		++s_items;
-		std::cout << "create " << m_uid << std::endl;
+		++items_;
+		printToConsole("Item " + std::to_string(uid_) + " created");
 	}
 
 	~Item()
 	{
-		--s_items;
-		std::cout << "delete " << m_uid << std::endl;
+		--items_;
+		printToConsole("Item " + std::to_string(uid_) + " deleted");
 	}
 
+	// returns the item's unique id
 	uint64_t uid() const
 	{
-		return m_uid;
+		return uid_;
 	}
 
-	long long lifeTime() const
+	// returns the item's lifetime until now
+	uint64_t lifeTime() const
 	{
-		return duration_cast<milliseconds>(system_clock::now() - m_spawntime).count();
+		return duration_cast<milliseconds>(system_clock::now() - spawntime_).count();
 	}
 
+	// returns the total number of Item objects constructed so far
 	static uint64_t items()
 	{
-		return s_items;
+		return items_;
 	}
 
 protected:
-	uint64_t m_uid;
-	system_clock::time_point m_spawntime;
-
-	// s_items is used to count the number of instances of Item
-	static uint64_t s_items;
-
-	// counter for incrementing unique item ids
-	static uint64_t s_counter;
-
+	uint64_t uid_; // item id
+	system_clock::time_point spawntime_; // item creation time
+	static uint64_t items_; // items_ is used to count the number of instances of Item
+	static uint64_t counter_; // counter for incrementing unique item ids
 };
 
-uint64_t Item::s_items = 0;
-uint64_t Item::s_counter = 0;
-
-
-using Queue = std::queue<Item *>;
+uint64_t Item::items_ = 0;
+uint64_t Item::counter_ = 1;
 
 class Consumer
 {
 public:
-	Consumer(Queue & queue) : m_queue{ queue }, m_terminate{ false } { }
+	Consumer(ThreadSafeQueue<Item*> &queue) : queue_{queue}, terminate_{false} { }
 
 	~Consumer()
 	{
@@ -76,7 +136,7 @@ public:
 
 	void work()
 	{
-		while (!m_terminate)
+		while (!terminate_)
 		{
 			// make worker thread sleep for short random time
 			std::this_thread::sleep_for(milliseconds(rand() % 200 + 1));
@@ -84,6 +144,7 @@ public:
 			cleanup();
 			consume();
 		}
+		printToConsole("TERMINATED");
 	}
 
 	void consume()
@@ -105,48 +166,47 @@ public:
 
 	void terminate()
 	{
-		std::cout << "\t\tTERMINATE" << std::endl;
-		m_terminate = true;
+		printToConsole("TERMINATE CONSUMER");
+		terminate_ = true;
 	}
 
 protected:
-	Queue & m_queue;
-	std::list<Item *> m_todo;
-
-	bool m_terminate;
+	ThreadSafeQueue<Item*> &queue_; // reference to the producer and the source of items
+	std::list<Item*> todo_; // own list of items currently being processed
+	bool terminate_; // flag that controls the termination of the consumer thread
 };
 
 
 int main(int /*argc*/, char * /*argv*/[])
 {
-	Queue queue; // queue of items that are to be processed/consumed by worker thread
+	ThreadSafeQueue<Item*> queue; // queue of items that are to be processed/consumed by worker thread
 
-	// instanciate a single Consumer item and run its work method in a local thread, the consumer thread
-	auto consumer = new Consumer{ queue };
+	// instantiate a single Consumer item and run its work method in a local thread, the consumer thread
+	auto consumer = new Consumer{queue};
 	std::thread t_work(&Consumer::work, consumer);
 
 	// reset random generator, using the system time
 	srand(static_cast<unsigned int>(time(0)));
 
+	// in the loop, the producer produces item objects
 	auto prob = 500;
 	while (rand() % --prob) // prob is used as a hack to randomly and deterministically terminate 
 	{
 		// ToDo: make producer thread sleep for short random time
-		
-		// ToDo: we generate a small number of items that are to be processed by the consumer 
-		
+
+		// ToDo: we generate a small number of items that are to be processed by the consumer
+
 	}
-	std::cout << std::endl << "TERMINATE " << std::endl;
+	printToConsole("TERMINATED");
 
 	// ToDo: delete remaining items, i.e., items that are still in the queue 
 	// and are not processed by the consumer; they got lost.
 
-
+	// consumer threads has to be stopped as well
 	consumer->terminate(); // (soft)terminate the consumer thread -> see while loop in Cosumer::work
 	t_work.join(); // block until consumer thread has terminated
 
 	// ToDo: make sure to also delete items left in consumer
-
 
 	// number of items left should be 0 -> precisely counted by Items constructor and destructor
 	std::cout << std::endl << Item::items() << " items left in memory ..." << std::endl;
